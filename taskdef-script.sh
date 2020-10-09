134f76fa850eabf8b7df56d35d0624b38ceb6cf9
Ajay Kumar <ajay011.sharma@hotmail.com>
2020-10-09 18:36:38 +0530

    Update taskdef-script.sh

--- taskdef-script.sh
+++ taskdef-script.sh
@@ -9,7 +9,7 @@ sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" flask-signup.json > flask-signup-v_$
 aws ecs register-task-definition --family flask-signup --cli-input-json file://flask-signup-v_${BUILD_NUMBER}.json
 
 # Update the service with the new task definition and desired count
-TASK_REVISION=`aws ecs describe-task-definition --task-definition flask-signup | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
+TASK_REVISION=`aws ecs describe-task-definition --task-definition flask-signup | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//' | sed 's/,$//'`
 DESIRED_COUNT=`aws ecs describe-services --cluster ${CLUSTER_NAME} --services ${SERVICE_NAME} | egrep "desiredCount" | tr "/" " " | awk '{print $2}' | sed 's/,$//'`
 if [ "${DESIRED_COUNT}" == "0" ]; then
     DESIRED_COUNT="1"
